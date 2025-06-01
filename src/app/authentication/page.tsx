9a361f9b6f7866e5f07bc8bd1e0b82580e228617
Guilherme Kaua <guilhermekaua10@gmail.com>
2025-06-01 17:36:51 -0300

    chore: add better auth setup

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,17 +1,7 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SignUpForm from "./components/sign-up-form";
+import LoginForm from "./components/login-form";
 
 const AutenticationPage = () => {
   return (
@@ -22,16 +12,7 @@ const AutenticationPage = () => {
           <TabsTrigger value="register">Criar conta</TabsTrigger>
         </TabsList>
         <TabsContent value="login">
-          <Card>
-            <CardHeader>
-              <CardTitle>Login</CardTitle>
-              <CardDescription>Faça login para continuar.</CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-6"></CardContent>
-            <CardFooter>
-              <Button>Entrar</Button>
-            </CardFooter>
-          </Card>
+          <LoginForm />
         </TabsContent>
         <TabsContent value="register">
           <SignUpForm />

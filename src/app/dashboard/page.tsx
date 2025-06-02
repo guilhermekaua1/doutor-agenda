c7b8ceb9c2dbd5972ba2b95add6b68027225ffbb
Guilherme Kaua <guilhermekaua10@gmail.com>
2025-06-02 01:12:43 -0300

    feat: add clinic form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,9 @@ import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
 import { redirect } from "next/navigation";
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 const DashboardPage = async () => {
   const session = await auth.api.getSession({
@@ -11,6 +14,13 @@ const DashboardPage = async () => {
   });
   if (!session?.user) {
     redirect("/authentication");
+  }
+  // preciso pegar as clínicas do usuário
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length == 0) {
+    redirect("/clinic-form");
   }
   return (
     <div>
